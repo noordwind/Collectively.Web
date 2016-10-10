19650aecd6c93f2f337c74b067bf52ce7256917d
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-10 10:36:50 +0200

    Displaying loader while fetching the remarks, resolved #8

--- src/resources/features/remarks/remarks.js
+++ src/resources/features/remarks/remarks.js
@@ -2,14 +2,18 @@ import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import LocationService from 'resources/services/location-service';
 import RemarkService from 'resources/services/remark-service';
+import LoaderService from 'resources/services/loader-service';
+import ToastService from 'resources/services/toast-service';
 import {EventAggregator} from 'aurelia-event-aggregator';
 
-@inject(Router, LocationService, RemarkService, EventAggregator)
+@inject(Router, LocationService, RemarkService, LoaderService, ToastService, EventAggregator)
 export class Remarks {
-    constructor(router, locationService, remarkService, eventAggregator) {
+    constructor(router, locationService, remarkService, loader, toast, eventAggregator) {
         this.router = router;
         this.locationService = locationService;
         this.remarkService = remarkService;
+        this.loader = loader;
+        this.toast = toast;
         this.eventAggregator = eventAggregator;
         this.radius = 1000;
         this.location = {};
@@ -20,12 +24,16 @@ export class Remarks {
     }
 
     async activate(){
+        this.loader.display();
         await this.eventAggregator.subscribe('map:loaded', async response => {
+            this.toast.info("Fetching the remarks...");
             this.locationService.getLocation(async location => {
                 this.location = location.coords;
                 this.query.longitude = this.location.longitude;
                 this.query.latitude = this.location.latitude;
                 await this.browse();
+                this.loader.hide();
+                this.toast.info("Remarks have been fetched.");
             });
         })
     }
