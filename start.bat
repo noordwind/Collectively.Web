2afe1d03c78d3e7a233c518355bc7ac5cba831d5
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-27 09:04:00 +0200

    Improved start scripts

--- start.bat
+++ start.bat
@@ -1,3 +1,6 @@
-node node_modules/.bin/r.js -o rbuild.js
-node node_modules/aurelia-cli/bin/aurelia-cli.js build --env $1
+IF "%1"=="" ( SET "APP_ENV=local" ) ELSE ( SET "APP_ENV=%1" )
+IF "%2"=="" ( SET "WATCH=--watch" ) ELSE ( SET "WATCH=%2" )
+echo Starting server using environment: $APP_ENV %WATCH...
+.cmd enode node_modules/.bin/r.js -o rbuild.js
+node node_modules/aurelia-cli/bin/aurelia-cli.js build --env %APP_ENV% %WATCH
 node node_modules/pushstate-server/bin/pushstate-server . 9000
--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+APP_ENV=${1:-local}
+WATCH=${2:---watch}
+if [ $APP_ENV == "-n" ]
+	then
+		WATCH="-n"
+		APP_ENV=local
+		echo $APP_ENV
+fi
+if [ $WATCH == "-n" ]
+	then
+		WATCH=""
+fi
+echo Starting server using environment: $APP_ENV $WATCH...
 node node_modules/.bin/r.js -o rbuild.js
-node node_modules/aurelia-cli/bin/aurelia-cli.js build --env $1
+node node_modules/aurelia-cli/bin/aurelia-cli.js build --env $APP_ENV $WATCH
 node node_modules/pushstate-server/bin/pushstate-server . 9000
