71bd5041511aebd78928677ad624914bbd94fbd8
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-27 09:20:05 +0200

    Improved start scripts

--- start.bat
+++ start.bat
@@ -1,6 +1,15 @@
 IF "%1"=="" ( SET "APP_ENV=local" ) ELSE ( SET "APP_ENV=%1" )
-IF "%2"=="" ( SET "WATCH=--watch" ) ELSE ( SET "WATCH=%2" )
-echo Starting server using environment: $APP_ENV %WATCH...
-.cmd enode node_modules/.bin/r.js -o rbuild.js
-node node_modules/aurelia-cli/bin/aurelia-cli.js build --env %APP_ENV% %WATCH
-node node_modules/pushstate-server/bin/pushstate-server . 9000
+IF "%2"=="" ( SET "WATCH=-w" ) ELSE ( SET "WATCH=%2" )
+if "%APP_ENV"=="-n" (
+	SET "WATCH="-n"
+	SET "APP_ENV=local"
+)
+node node_modules/.bin/r.js -o rbuild.js
+IF "%WATCH"=="-w" (
+	node node_modules/aurelia-cli/bin/aurelia-cli.js run --env %APP_ENV --watch
+)
+ELSE (
+	node node_modules/aurelia-cli/bin/aurelia-cli.js build --env %APP_ENV
+	node node_modules/pushstate-server/bin/pushstate-server . 9000
+)
+
--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 APP_ENV=${1:-local}
-WATCH=${2:---watch}
+WATCH=${2:--w}
 if [ $APP_ENV == "-n" ]
 	then
 		WATCH="-n"
 		APP_ENV=local
-		echo $APP_ENV
 fi
-if [ $WATCH == "-n" ]
-	then
-		WATCH=""
-fi
-echo Starting server using environment: $APP_ENV $WATCH...
 node node_modules/.bin/r.js -o rbuild.js
-node node_modules/aurelia-cli/bin/aurelia-cli.js build --env $APP_ENV $WATCH
-node node_modules/pushstate-server/bin/pushstate-server . 9000
+if [ $WATCH == "-w" ]
+	then
+		node node_modules/aurelia-cli/bin/aurelia-cli.js run --env $APP_ENV --watch
+	else
+		node node_modules/aurelia-cli/bin/aurelia-cli.js build --env $APP_ENV
+		node node_modules/pushstate-server/bin/pushstate-server . 9000
+fi
