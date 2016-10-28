743bb391654742f6c1b8c9287dd3074c0c94b73c
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-28 13:28:30 +0200

    Local & docker dockerfiles

---
+++ aurelia_project/environments/docker.js
@@ -0,0 +1,16 @@
+export default {
+  debug: true,
+  testing: true,
+  title: 'Coolector [Docker]',
+  apiUrl: 'http://api:5000/',
+  idTokenStorageKey: 'idToken',
+  accessTokenStorageKey: 'accessToken',
+  profileStorageKey: 'profile',
+  filtersStorageKey: 'filters',
+  locationStorageKey: 'location',
+  auth0: {
+    token: 'eYnnpDd1k61vxXQCbFwWtX45yX3PxFDA',
+    domain: 'noordwind-dev.eu.auth0.com',
+    jwtExpiration: 36000
+  }
+};
