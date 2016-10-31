232bb56a4abb8a85618b6159dd41335894de5b62
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-10-31 10:59:40 +0100

    categories filter

--- src/resources/features/remarks/filter-remarks.js
+++ src/resources/features/remarks/filter-remarks.js
@@ -3,19 +3,31 @@ import {Router} from 'aurelia-router';
 import LocationService from 'resources/services/location-service';
 import FiltersService from 'resources/services/filters-service';
 import ToastService from 'resources/services/toast-service';
+import RemarkService from 'resources/services/remark-service';
 
-@inject(Router, LocationService, FiltersService, ToastService)
+@inject(Router, LocationService, FiltersService, ToastService, RemarkService)
 export class FilterRemarks {
-    constructor(router, locationService, filtersService, toast) {
+    constructor(router, locationService, filtersService, toast, remarkService) {
         this.router = router;
         this.location = locationService;
         this.filtersService = filtersService;
         this.toast = toast;
+        this.remarkService = remarkService;
         this.filters = this.filtersService.filters;
     }
 
     async activate() {
-        this.location.startUpdating();
+        let self = this;
+        self.location.startUpdating();
+        let categories = await self.remarkService.getCategories();
+        categories.forEach(function(category){
+            if(self.filters.categories === null || typeof(self.filters.categories) === "undefined"){
+                category.checked = true;
+            } else {
+                category.checked = self.filters.categories.indexOf(category.name) !== -1;
+            }
+        });
+        self.categories = categories;
     }
 
     resetFilters() {
@@ -24,7 +36,13 @@ export class FilterRemarks {
     }
 
     filterRemarks() {
+        this.filters.categories = this.selectedCategories;
         this.filtersService.filters = this.filters;
         this.router.navigateBack();
+    }
+
+    get selectedCategories(){
+        return $.grep(this.categories, c => c.checked)
+            .map(c => c.name);
     }
 }
