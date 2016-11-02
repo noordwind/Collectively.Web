55b6ab0081bc726e468790dc177a11ed14b53a48
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-11-02 13:43:07 +0100

    remark.js formatted and fixed

--- src/resources/features/remarks/remark.js
+++ src/resources/features/remarks/remark.js
@@ -6,61 +6,60 @@ import ToastService from 'resources/services/toast-service';
 import LoaderService from 'resources/services/loader-service';
 import AuthService from 'resources/services/auth-service';
 import {EventAggregator} from 'aurelia-event-aggregator';
-import environment from '../../../environment';
 
 @inject(Router, LocationService, RemarkService, ToastService, LoaderService, AuthService, EventAggregator)
 export class Remark {
-    constructor(router, location, remarkService, toastService, loader, authService, eventAggregator) {
-        this.router = router;
-        this.location = location;
-        this.remarkService = remarkService;
-        this.toast = toastService;
-        this.loader = loader;
-        this.authService = authService;
-        this.eventAggregator = eventAggregator;
-        this.remark = {};
-        this.isDeleting = false;
-        this.isInRange = false;
-    }
+  constructor(router, location, remarkService, toastService, loader, authService, eventAggregator) {
+    this.router = router;
+    this.location = location;
+    this.remarkService = remarkService;
+    this.toast = toastService;
+    this.loader = loader;
+    this.authService = authService;
+    this.eventAggregator = eventAggregator;
+    this.remark = {};
+    this.isDeleting = false;
+    this.isInRange = false;
+  }
 
-    get canDelete() {
-        let profile = JSON.parse(this.authService.profile);
+  get canDelete() {
+    let profile = this.authService.profile;
 
-        return profile.user_id === this.remark.author.userId;
-    }
+    return profile.user_id === this.remark.author.userId;
+  }
 
-    get canResolve() {
-        return this.remark.resolved === false && this.isInRange;
-    }
+  get canResolve() {
+    return this.remark.resolved === false && this.isInRange;
+  }
 
-    async activate(params, routeConfig) {
-        this.location.startUpdating();
-        this.id = params.id;
-        let remark = await this.remarkService.getRemark(this.id);
-        this.remark = remark;
-        this.mediumPhoto = remark.photos.find(x => x.size === 'medium');
-        this.bigPhoto = remark.photos.find(x => x.size === 'big');
-        this.resolveUrl = this.router.generate('resolve-remark', {id: remark.id});
-        this.resolvedMediumPhoto = remark.photos.find(x => x.size === 'medium' && x.metadata === 'resolved');
-        this.resolvedBigPhoto = remark.photos.find(x => x.size === 'big' && x.metadata === 'resolved');
-        this.state = remark.resolved ? 'resolved' : 'new';
-        this.isInRange = this.location.isInRange({
-            latitude : remark.location.coordinates[1],
-            longitude : remark.location.coordinates[0]
-        });
-    }
+  async activate(params, routeConfig) {
+    this.location.startUpdating();
+    this.id = params.id;
+    let remark = await this.remarkService.getRemark(this.id);
+    this.remark = remark;
+    this.mediumPhoto = remark.photos.find(x => x.size === 'medium');
+    this.bigPhoto = remark.photos.find(x => x.size === 'big');
+    this.resolveUrl = this.router.generate('resolve-remark', {id: remark.id});
+    this.resolvedMediumPhoto = remark.photos.find(x => x.size === 'medium' && x.metadata === 'resolved');
+    this.resolvedBigPhoto = remark.photos.find(x => x.size === 'big' && x.metadata === 'resolved');
+    this.state = remark.resolved ? 'resolved' : 'new';
+    this.isInRange = this.location.isInRange({
+      latitude: remark.location.coordinates[1],
+      longitude: remark.location.coordinates[0]
+    });
+  }
 
-    async delete() {
-        if (this.canDelete === false) {
-            await this.toast.error('I\'m sorry. You are not allowed to delete a remark!');
-            return;    
-        }
-        this.loader.display();
-        this.isDeleting = true;
-        await this.remarkService.deleteRemark(this.id);
-        await this.toast.info('Delete request has been sent, please wait...');
-        this.isDeleting = false;
-        this.loader.hide();
-        this.router.navigate('remarks');
+  async delete() {
+    if (this.canDelete === false) {
+      await this.toast.error('I\'m sorry. You are not allowed to delete a remark!');
+      return;
     }
+    this.loader.display();
+    this.isDeleting = true;
+    await this.remarkService.deleteRemark(this.id);
+    await this.toast.info('Delete request has been sent, please wait...');
+    this.isDeleting = false;
+    this.loader.hide();
+    this.router.navigate('remarks');
+  }
 }
