c6c369464de9edb68f9a0cfee62b94e5ac057186
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-11-22 14:09:16 +0100

    publish message on event aggregator when received from signalr

--- src/resources/services/signalr-service.js
+++ src/resources/services/signalr-service.js
@@ -1,18 +1,30 @@
 import environment from '../../environment';
+import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
+@inject(EventAggregator)
 export default class SignalRService {
-  constructor() {
+  constructor(eventAggregator) {
+    this.eventAggregator = eventAggregator;
     this.connection = null;
   }
 
   initialize() {
-    if (this.connection === null) {
-      this.connection = new RpcConnection(`${environment.signalRUrl}remarks`, 'formatType=json&format=text');
-      this.connection.on('RemarkCreated', (message) => {
-        console.log('Remark created ' + JSON.stringify(message));
-      });
-
-      this.connection.start();
+    if (this.connection !== null) {
+      return;
     }
+
+    this.connection = new RpcConnection(`${environment.signalRUrl}remarks`, 'formatType=json&format=text');
+    this.connection.on('RemarkCreated', (message) => {
+      this.eventAggregator.publish('remark:created', message);
+    });
+    this.connection.on('RemarkResolved', (message) => {
+      this.eventAggregator.publish('remark:resolved', message);
+    });
+    this.connection.on('RemarkDeleted', (message) => {
+      this.eventAggregator.publish('remark:deleted', message);
+    });
+
+    this.connection.start();
   }
 }
