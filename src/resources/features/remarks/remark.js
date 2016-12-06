ec57ccbaad0478df9e9fa93597924ba2c0440016
spetz <spetz90@gmail.com>
2016-12-06 09:04:47 +0100

    Remark translations

--- src/resources/features/remarks/remark.js
+++ src/resources/features/remarks/remark.js
@@ -1,5 +1,7 @@
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
+import {I18N} from 'aurelia-i18n';
+import TranslationService from 'resources/services/translation-service';
 import LocationService from 'resources/services/location-service';
 import FiltersService from 'resources/services/filters-service';
 import RemarkService from 'resources/services/remark-service';
@@ -10,13 +12,15 @@ import SignalRService from 'resources/services/signalr-service';
 import {EventAggregator} from 'aurelia-event-aggregator';
 import Environment from '../../../environment';
 
-
-@inject(Router, LocationService, FiltersService, RemarkService,
+@inject(Router, I18N, TranslationService,
+LocationService, FiltersService, RemarkService,
 ToastService, LoaderService, UserService,
 SignalRService, EventAggregator, Environment)
 export class Remark {
-  constructor(router, location, filtersService, remarkService, toastService, loader, userService, signalR, eventAggregator, environment) {
+  constructor(router, i18n, translationService, location, filtersService, remarkService, toastService, loader, userService, signalR, eventAggregator, environment) {
     this.router = router;
+    this.i18n = i18n;
+    this.translationService = translationService;
     this.location = location;
     this.filtersService = filtersService;
     this.filters = this.filtersService.filters;
@@ -48,11 +52,13 @@ export class Remark {
     this.account = await this.userService.getAccount();
     let remark = await this.remarkService.getRemark(this.id);
     this.remark = remark;
+    this.remark.categoryName = this.translationService.tr(`remark.category_${this.remark.category.name}`);
     this.mediumPhoto = remark.photos.find(x => x.size === 'medium');
     this.bigPhoto = remark.photos.find(x => x.size === 'big');
     this.resolvedMediumPhoto = remark.photos.find(x => x.size === 'medium' && x.metadata === 'resolved');
     this.resolvedBigPhoto = remark.photos.find(x => x.size === 'big' && x.metadata === 'resolved');
     this.state = remark.resolved ? 'resolved' : 'new';
+    this.stateName = this.translationService.tr(`remark.state_${this.state}`);
     this.latitude = remark.location.coordinates[1];
     this.longitude = remark.location.coordinates[0];
     this.isInRange = this.location.isInRange({
@@ -75,7 +81,7 @@ export class Remark {
         };
         this.remark.resolvedAt = message.resolvedAt;
         if (this.account.userId !== this.remark.resolver.userId) {
-          this.toast.success('Remark has been resolved.');
+          this.toast.success(this.translationService.tr('remark.remark_resolved'));
         }
       });
     this.remarkDeletedSubscription = await this.eventAggregator
@@ -84,7 +90,7 @@ export class Remark {
           return;
         }
         if (this.account.userId !== this.remark.author.userId) {
-          this.toast.info('Remark has been removed by the author');
+          this.toast.info(this.translationService.tr('remark.remark_removed_by_author'));
           this.router.navigateToRoute('remarks');
         }
       });
@@ -105,15 +111,17 @@ export class Remark {
 
   async delete() {
     if (this.canDelete === false) {
-      await this.toast.error('I\'m sorry. You are not allowed to delete a remark!');
+      await this.toast.error(this.translationService.tr('remark.not_allowed_to_delete'));
+
       return;
     }
     this.loader.display();
     this.isDeleting = true;
-    this.toast.info('Removing remark, please wait...');
+    this.toast.info(this.translationService.tr('remark.removing_remark'));
     let remarkRemoved = await this.remarkService.deleteRemark(this.id);
     if (remarkRemoved.success) {
-      this.toast.success('Remark has been removed.');
+      this.toast.success(this.translationService.tr('remark.remark_removed')
+);
       this.loader.hide();
       this.router.navigateToRoute('remarks');
 
@@ -133,10 +141,10 @@ export class Remark {
     };
     this.isSending = true;
     this.loader.display();
-    this.toast.info('Resolving remark, please wait...');
+    this.toast.info(this.translationService.tr('remark.resolving_remark'));
     let remarkResolved = await this.remarkService.resolveRemark(command);
     if (remarkResolved.success) {
-      this.toast.success('Remark has been resolved.');
+      this.toast.success(this.translationService.tr('remark.remark_resolved'));
       this.loader.hide();
       this.router.navigateToRoute('remarks');
 
