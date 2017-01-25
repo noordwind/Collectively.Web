3277a898056f9c89b44db13ebdf4301cfd354396
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-01-25 15:14:51 +0100

    display hours from 00 to 23

--- src/resources/converters/dateFormatValueConverter.js
+++ src/resources/converters/dateFormatValueConverter.js
@@ -2,6 +2,6 @@ import * as moment from 'moment';
 
 export class DateFormatValueConverter {
    toView(value) {
-      return moment.default(value).format('DD/MM/YYYY hh:mm');
+      return moment.default(value).format('DD/MM/YYYY HH:mm');
    }
 }
