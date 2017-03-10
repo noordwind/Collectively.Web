7e16ecbf1c85e57766e242889fa2cf89c553318c
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-03-10 11:41:15 +0100

    websocket-service handlers updated

--- src/resources/services/websocket-service.js
+++ src/resources/services/websocket-service.js
@@ -11,7 +11,7 @@ export default class WebsocketService {
     this.eventAggregator = eventAggregator;
     this.authService = authService;
     this.logger = logService;
-    this.connection = null;
+    this.socket = null;
     this.reconnect = true;
   }
 
@@ -23,51 +23,49 @@ export default class WebsocketService {
       return;
     }
 
-    this.connect();
-    this.connection.on('operation_updated', (message) => {
-      this.logger.debug('operation_updated message received', message);
-      this.eventAggregator.publish('operation:updated', message);
-    });
-    this.connection.on('remark_created', (message) => {
-      this.logger.debug('remark_created message received', message);
-      this.eventAggregator.publish('remark:created', message);
-    });
-    this.connection.on('remark_resolved', (message) => {
-      this.logger.debug('remark_resolved message received', message);
-      this.eventAggregator.publish('remark:resolved', message);
-    });
-    this.connection.on('remark_deleted', (message) => {
-      this.logger.debug('remark_deleted message received', message);
-      this.eventAggregator.publish('remark:deleted', message);
-    });
-    this.connection.on('photos_to_remark_added', (message) => {
-      this.logger.debug('photos_to_remark_added message received', message);
-      this.eventAggregator.publish('remark:photo_added', message);
-    });
-    this.connection.on('photos_from_remark_removed', (message) => {
-      this.logger.debug('photos_from_remark_removed message received', message);
-      this.eventAggregator.publish('remark:photo_removed', message);
-    });
-    this.connection.on('remark_vote_submitted', (message) => {
-      this.logger.debug('remark_vote_submitted message received', message);
-      this.eventAggregator.publish('remark:vote_submitted', message);
-    });
-    this.connection.on('remark_vote_deleted', (message) => {
-      this.logger.debug('remark_vote_deleted message received', message);
-      this.eventAggregator.publish('remark:vote_deleted', message);
+    console.log('connecting to socket io server');
+    this.socket = io(environment.websocketUrl);
+    this.socket.on('connect', () => {
+      this.socket.on('operation_updated', (message) => {
+        this.logger.debug('operation_updated message received', message);
+        this.eventAggregator.publish('operation:updated', message);
+      });
+      this.socket.on('remark_created', (message) => {
+        this.logger.debug('remark_created message received', message);
+        this.eventAggregator.publish('remark:created', message);
+      });
+      this.socket.on('remark_resolved', (message) => {
+        this.logger.debug('remark_resolved message received', message);
+        this.eventAggregator.publish('remark:resolved', message);
+      });
+      this.socket.on('remark_deleted', (message) => {
+        this.logger.debug('remark_deleted message received', message);
+        this.eventAggregator.publish('remark:deleted', message);
+      });
+      this.socket.on('photos_to_remark_added', (message) => {
+        this.logger.debug('photos_to_remark_added message received', message);
+        this.eventAggregator.publish('remark:photo_added', message);
+      });
+      this.socket.on('photos_from_remark_removed', (message) => {
+        this.logger.debug('photos_from_remark_removed message received', message);
+        this.eventAggregator.publish('remark:photo_removed', message);
+      });
+      this.socket.on('remark_vote_submitted', (message) => {
+        this.logger.debug('remark_vote_submitted message received', message);
+        this.eventAggregator.publish('remark:vote_submitted', message);
+      });
+      this.socket.on('remark_vote_deleted', (message) => {
+        this.logger.debug('remark_vote_deleted message received', message);
+        this.eventAggregator.publish('remark:vote_deleted', message);
+      });
     });
   }
 
   get initalized() {
-    return this.connection !== null;
+    return this.socket !== null;
   }
 
   get connected() {
-    return this.connection !== null && this.connection.connected;
-  }
-
-  connect() {
-    console.log('connecting to socket io server');
-    this.connection = io.connect(environment.websocketUrl);
+    return this.socket !== null && this.socket.connected;
   }
 }
